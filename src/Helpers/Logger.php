298d04958c33933928b24662e30c479c9ea5d447
michel melo <michel.lander.melo@sibs.pt>
2025-05-19 15:14:08 +0100

    fix: Correct log file path and ensure log directory creation in Logger class

--- src/Helpers/Logger.php
+++ src/Helpers/Logger.php
@@ -5,7 +5,24 @@ namespace MichelMelo\PaymentGateway\Helpers;
 class Logger
 {
     private static $enabled = true; // Controle para ativar/desativar o log
-    private static $logFile = __DIR__ . '/'._PS_MODULE_DIR_.'/logs/payment-gateway.log';
+    private static $logFile = __DIR__ . '/logs/payment-gateway.log';
+
+    public function __construct($logFile = null)
+    {
+        if ($logFile) {
+            self::$logFile = $logFile;
+        }
+
+        // Verifica se o diretório de logs existe, caso contrário, cria
+        if (!file_exists(dirname(self::$logFile))) {
+            mkdir(dirname(self::$logFile), 0755, true);
+        }
+    
+        // Verifica se o arquivo de log existe, caso contrário, cria
+        if (!file_exists(self::$logFile)) {
+            file_put_contents(self::$logFile, '');
+        }
+    }
 
     /**
      * Ativa o sistema de log.
